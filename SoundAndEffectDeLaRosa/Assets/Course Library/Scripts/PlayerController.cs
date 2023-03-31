59e1deeae9e47a889e974bae35a9867c3e271971
delarosaii17 <tacaltacc@gmail.com>
2023-03-31 15:16:51 -0700

    finished unit 3
    I finished unit 3, now I just need to do bonus features, but everything else is finished

--- SoundAndEffectDeLaRosa/Assets/Course Library/Scripts/PlayerController.cs
+++ SoundAndEffectDeLaRosa/Assets/Course Library/Scripts/PlayerController.cs
@@ -7,11 +7,19 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private Animator playerAnim;
 
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
     public float jumpForce;
     public float gravityModifer;
 
     public bool isOnGround = true;
     public bool gameOver = false;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +27,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent <Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifer;
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,9 +35,11 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            dirtParticle.Stop();
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -36,14 +47,18 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            dirtParticle.Play();
             isOnGround = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            explosionParticle.Play();
+            dirtParticle.Stop();
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
